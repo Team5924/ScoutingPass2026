5ad8babccb8467827c21a490b0cffdd196d47b26
Marisa Hoosen <15793048+marisa07@users.noreply.github.com>
2026-02-20 20:09:51 -0800

    Auto send to google sheets (#1)
    Updated tba key, enabled send to gs button, switched data type to tsv

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -1,20 +1,20 @@
 function setUpGoogleSheets() {
-    const scriptURL = '<SCRIPT URL>'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxYpArzIdDBCy3ZZmjH2KZTErAfJWl0OsqWO_46VfBQaPsyqC0CuW3rL8fK_Qp80zE/exec'
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
  
     
     form.addEventListener('submit', e => {
       e.preventDefault()
-      btn.disabled = true
+      btn.disabled = false
       btn.innerHTML = "Sending..."
 
-      let fd = getData(false)
-      for (const [key, value] of fd) {
-        console.log(`${key}: ${value}\n`);
-      }
-
-      fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: fd })
+      let fd = getData("tsv");
+      fetch(scriptURL, {
+        method: "POST",
+        mode: 'no-cors',
+        body: fd
+      })
         .then(response => { 
               alert('Success!', response) })
         .catch(error => {
