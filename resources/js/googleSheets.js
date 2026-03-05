d2ccbd538f7bf1813d7b224c99fb6750360a3da6
Marisa Hoosen <15793048+marisa07@users.noreply.github.com>
2026-03-05 15:49:23 -0800

    Qual app (#9)

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -2,16 +2,19 @@ function setUpGoogleSheets() {
     const scriptURL = 'https://script.google.com/macros/s/AKfycbxYpArzIdDBCy3ZZmjH2KZTErAfJWl0OsqWO_46VfBQaPsyqC0CuW3rL8fK_Qp80zE/exec'
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
- 
-    
+
+    if (!form || !btn) return;
+
     form.addEventListener('submit', e => {
       e.preventDefault()
-      btn.disabled = false
+      btn.disabled = true
       btn.innerHTML = "Sending..."
 
       let fd = getData("tsv");
       if (pitScouting) {
-        fd = "PIT\t" + fd;
+          fd = "PIT\t" + fd;
+      } else if (typeof qualCollectData === 'function') {
+          fd = "QUAL\t" + qualCollectData();
       }
       fetch(scriptURL, {
         method: "POST",
@@ -27,3 +30,5 @@ function setUpGoogleSheets() {
       btn.innerHTML = "Send to Google Sheets"
     })
 }
+
+
