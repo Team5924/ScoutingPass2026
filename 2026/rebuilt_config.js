d4bc91464c5cddf02ebbe79018cb9dbb9c149c56
marisa07 <mhoosen2@gmail.com>
2026-03-06 22:43:10 -0800

    switch default event code

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025casf",
+      "defaultValue": "2026cahal",
       "required": "true"
     },
     { "name": "Match Level",
